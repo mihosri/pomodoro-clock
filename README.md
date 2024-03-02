2eeebe5e302828524d1dfdc9ccc89b7d3f3e9b83
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-02 10:23:35 -0700

    Implemented user stories 1 - 6

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pomodoro-clock
+
 clock that has 25 min session and 5 min as break by default
+
+# Technologies
+
+- Vite
+- TypeScript
+- React
+- CSS
+
+# How to run this application
+
+cd pomodoro-clock
+npm install
+npm run dev
+
+# Challenges and Success
+
+-
+
+# Learnings
+
+- fixed a bug by replacing the variable name from break (keyword) to breakTime
+-
+
+# Deployed App Link
--- pomodoro-clock/src/App.tsx
+++ pomodoro-clock/src/App.tsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [breakTime, setBreakTime] = useState(5)
+  const [session, setSession] = useState(25)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Pomodoro Clock</h1>
+      <div className="break-container">
+        <div id="break-label">"Break Length"</div>
+        <button id="break-decrement"></button>
+        <div id="break-length">{breakTime}</div>
+        <button id="break-increment"></button>
+      </div>
+
+      <div className="session-container">
+        <div id="session-label">"session Length"</div>
+        <button id="session-decrement"></button>
+        <div id="session-length">{session}</div>
+        <button id="session-increment"></button>
+      </div>
+    </div>
   )
 }
 
